662a1b7cea4af30001eb7995155b6a4dec54907a
eastWillow <bobgash2@gmail.com>
2017-01-16 06:28:33 +0800

    malloc done

--- BinarySearchTree/main.c
+++ BinarySearchTree/main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _Node
 {
@@ -10,13 +11,18 @@ typedef struct _Node
 } Node;
 
 uint8_t main(void) {
+	/*
 	Node node1;
 	void* node1Ptr;
 	node1Ptr = &node1;
 	node1.data = 10;
+	*/
+
+	Node* node1Ptr = (Node*)malloc(sizeof(Node));
+	node1Ptr->data = 10;
 
 	printf("HelloWorld\n");
-	printf("node.data = %d\n", node1.data);
+	printf("node.data = %d\n", node1Ptr->data);
 	printf("nodePtr = %x\n", (intptr_t)node1Ptr);
 	getchar();
 	return 0;
