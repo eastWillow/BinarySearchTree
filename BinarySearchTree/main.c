610b098c45d9c4e7af11c9904aba5a506b3a3987
bobgash2 <bobga@DESKTOP-IETOAFH>
2017-01-21 01:28:00 +0800

    fix pointer to pointer problem

--- BinarySearchTree/main.c
+++ BinarySearchTree/main.c
@@ -13,20 +13,20 @@ typedef struct _Node
 } Node;
 
 uint8_t data[100] = {0,1,3,6,7,4,2,5,8,9};
-Node* root = NULL;
 
-void createTree(_In_ uint8_t* data, size_t dataSize);
+void createTree(_Out_ Node* *parents,_In_ uint8_t* data, size_t dataSize);
 Node* compare(_In_ Node* parents, uint8_t newData);
 void printTree(_In_ Node* root);
 
 uint8_t main(void) {
-	
+	Node* root = NULL;
 	//create random
 	srand(time(NULL));
 	for (int i = 0; i < sizeof(data); i++) {
-		data[i] = rand()% ((int)sizeof(data) +1);
+		//data[i] = rand()% ((int)sizeof(data) +1);
+		data[i] = rand() % (10 + 1);
 	}
-	createTree(data, sizeof(data));
+	createTree(&root, data, sizeof(data));
 	printTree(root);
 
 	//printf("\n%p", &nodeMap[0]);
@@ -35,18 +35,16 @@ uint8_t main(void) {
 	return 0;
 }
 
-void createTree(_In_ uint8_t* data, size_t dataSize) {
-	//create root
-
-	root = (Node*)malloc(sizeof(Node));
-	root->parent = NULL;
-	root->left = NULL;
-	root->right = NULL;
-	root->same = 0;
-	root->data = data[0];
+void createTree(_Out_ Node* *parents,_In_ uint8_t* data, size_t dataSize) {
+	((Node*)*parents) = (Node*)malloc(sizeof(Node));
+	((Node*)*parents)->parent = NULL;
+	((Node*)*parents)->left = NULL;
+	((Node*)*parents)->right = NULL;
+	((Node*)*parents)->same = 0;
+	((Node*)*parents)->data = data[0];
 
 	for (int i = 1; i < dataSize; i++) {
-		compare(root, data[i]);
+		compare(((Node*)*parents), data[i]);
 	}
 }
 
