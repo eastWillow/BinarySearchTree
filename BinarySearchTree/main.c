2adedb6768c62f9cb5e85f983c366dd05b1e2718
eastWillow <bobgash2@gmail.com>
2017-01-20 14:58:33 +0800

    create Tree

--- BinarySearchTree/main.c
+++ BinarySearchTree/main.c
@@ -10,20 +10,40 @@ typedef struct _Node
 	int data;
 } Node;
 
+Node *nodeMap[10];
+
+void newLeaf(_In_ uint8_t* data, _Inout_ Node *map[]);
+
 uint8_t main(void) {
-	/*
-	Node node1;
-	void* node1Ptr;
-	node1Ptr = &node1;
-	node1.data = 10;
-	*/
+	uint8_t data[] = { 5,1,2,7,9 };
+	newLeaf(data, nodeMap);
 
-	Node* node1Ptr = (Node*)malloc(sizeof(Node));
-	node1Ptr->data = 10;
-
-	printf("HelloWorld\n");
-	printf("node.data = %d\n", node1Ptr->data);
-	printf("nodePtr = %x\n", (intptr_t)node1Ptr);
+	printf("%p", &nodeMap[0]);
 	getchar();
 	return 0;
+}
+
+void createTree(_In_ uint8_t* data, _Inout_ Node *map[]) {
+	//create root
+
+	map[0] = (Node*)malloc(sizeof(Node));
+	map[0]->parent = NULL;
+	map[0]->data = data[0];
+
+	for (int i = 1; i < sizeof(data); i++) {
+		if (map[i-1]->data < data[i]) {
+			if (map[i-1]->left == NULL) {
+				map[i] = (Node*)malloc(sizeof(Node));
+				map[i - 1]->left = map[i];
+				map[i]->parent = map[i - 1];
+				map[i]->data = data[i];
+			}
+			else {
+
+			}
+		}
+		else {
+
+		}
+	}
 }
